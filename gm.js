2a403537d46f574990b506753e6c55977500d9b9
rafael <rafquadros22@gmail.com>
2020-05-19 15:23:30 -0400

    lorem commit2

--- gm.js
+++ gm.js
@@ -7,7 +7,7 @@ function generateMark(data) {
   fs.writeFile(
     filename,
 
-    ` [![Generic badge](https://img.shields.io/badge/<Github>-<Repo>-<COLOR>.svg)](https://github.com/Rafquadros22) \n\n ${"# " + data.title} \n\n [${data.tableOfContents[0]}] \n [${data.tableOfContents[1]}] \n [${data.tableOfContents[2]}] \n [${ data.tableOfContents[3]}]
+    ` [![Generic badge](https://img.shields.io/badge/<Github>-<Repo>-<COLOR>.svg)](https://github.com/Rafquadros22) \n\n ${"# " + data.title} \n\n [${data.tableOfContents[0]}](#Installation) \n [${data.tableOfContents[1]}] \n [${data.tableOfContents[2]}] \n [${ data.tableOfContents[3]}]
       \n\n ## Description \n ${data.description} \n\n ## Installation \n ${data.installation} \n\n ## Credits \n ${data.credits} \n\n ## Usage \n ${data.usage} \n\n ## License \n ${data.license}
     `,
 
@@ -23,3 +23,4 @@ function generateMark(data) {
 }
 
 module.exports = generateMark;
+
